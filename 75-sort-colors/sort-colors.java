6777ce4d3a374ee791db1de9c17a022038952c5f
Arnabzone <arnabworkinfo@gmail.com>
2025-07-10 15:44:34 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (67.98%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,13 +1,36 @@
 class Solution {
     public void sortColors(int[] a) {
-        int c0=0,c1=0,c2=0;
-        for(int num: a){
-            if(num==0) c0++;
-            else if(num==1) c1++;
-            else c2++;
+        // 0(n) TC
+    //     int c0=0,c1=0,c2=0;
+    //     for(int num: a){
+    //         if(num==0) c0++;
+    //         else if(num==1) c1++;
+    //         else c2++;
+    //     }
+    //     for(int i=0;i<c0;i++) a[i]=0;
+    //     for(int i=c0;i<c0+c1;i++) a[i]=1;
+    //     for(int i=c0+c1;i<a.length;i++) a[i]=2;
+    // }
+
+    // 0(1)
+
+    int low=0,mid=0,high=a.length-1;
+    while(mid<=high){
+        if(a[mid]==0){
+            int temp=a[low];
+            a[low]=a[mid];
+            a[mid]=temp;
+            low++;
+            mid++;
         }
-        for(int i=0;i<c0;i++) a[i]=0;
-        for(int i=c0;i<c0+c1;i++) a[i]=1;
-        for(int i=c0+c1;i<a.length;i++) a[i]=2;
+        else if(a[mid]==1){
+            mid++;
+        }
+        else {
+            int temp=a[mid];
+            a[mid]=a[high];
+            a[high]=temp;
+            high--;
+        }
     }
-}
+}}
