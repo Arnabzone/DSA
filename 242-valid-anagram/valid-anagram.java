3288cfa0f5ad974b6ee24ced51bb7b17eb4a4534
Arnabzone <arnabworkinfo@gmail.com>
2026-02-10 13:14:12 +0530

    Time: 5 ms (58.59%) | Memory: 44.5 MB (74.85%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,17 @@
-public class Solution {
+class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+        if(s.length()!=t.length()){
             return false;
         }
-
-        HashMap<Character, Integer> countS = new HashMap<>();
-        HashMap<Character, Integer> countT = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
-            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        int hash[]= new int [26];
+        for(int i=0;i<s.length();i++){
+            hash[s.charAt(i)-'a']++;
+            hash[t.charAt(i)-'a']--;
         }
-        return countS.equals(countT);
+        for(int val:hash){
+            if(val!=0){
+                return false;}
+        }
+        return true;
     }
 }
