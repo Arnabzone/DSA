67dc0e9cd7753ea304a96bc271eae8734ec87dec
Arnabzone <arnabworkinfo@gmail.com>
2026-08-27 16:41:24 +0530

    Time: 1146 ms (97.02%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query statement below
-SELECT p.product_name,s.year,s.price
-FROM Product p
-INNER JOIN Sales s
-ON p.product_id=s.product_id; 
+SELECT p.product_name,s.year,s.price 
+FROM
+Sales AS s
+INNER JOIN
+Product AS p
+ON s.product_id=p.product_id;
